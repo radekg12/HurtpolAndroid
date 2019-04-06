9d85cea5ccec9f6640cd6872767a2cd5d8624b1e
jargaw12 <33065466+jargaw12@users.noreply.github.com>
2019-04-07 00:31:21 +0200

    add card menu

---
+++ app/src/main/java/com/example/hurtpolandroid/ui/cardmenu/CardMenuActivity.kt
@@ -0,0 +1,69 @@
+package com.example.hurtpolandroid.ui.cardmenu
+
+import android.os.Bundle
+import com.google.android.material.navigation.NavigationView
+import androidx.core.view.GravityCompat
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.example.hurtpolandroid.R
+import kotlinx.android.synthetic.main.activity_card_menu.*
+import kotlinx.android.synthetic.main.app_bar_card_menu.*
+import kotlinx.android.synthetic.main.content_card_menu.*
+
+class CardMenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_menu)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+        card1.setOnClickListener {
+            //TODO add card1 click action
+        }
+        card1.setOnClickListener {
+            //TODO add card2 click action
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.card_menu, menu)
+        return true
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.logout -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
